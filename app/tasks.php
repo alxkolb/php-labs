63377be8723f8e41b041183768cb8c0017438d08
Alexander Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-11-19 23:36:53 +0300

    Задание 2

--- app/tasks.php
+++ app/tasks.php
@@ -27,3 +27,37 @@ class Task1 {
         echo "Площадь = " . self::s($val) . "<br/>Объём = " . self::v($val);
     }
 }
+
+/**
+ * Даны три положительных числа. Проверить, могут ли они быть длинами сторон треугольника.
+ * Если да, то вычислить радиус вписанной окружности.
+ */
+class Task2 {
+    function isTriangle($a, $b, $c) {
+        return ($a + $b > $c) && ($a + $c > $b) && ($b + $c > $a);
+    }
+
+    /** Полупериметр */
+    private function p($a, $b, $c) {
+        return ($a + $b + $c) / 2;
+    }
+
+    function getRadius($a, $b, $c) {
+        if (!self::isTriangle($a, $b, $c)) return null;
+
+        $p = self::p($a, $b, $c);
+        return sqrt($p * ($p - $a) * ($p - $b) * ($p - $c));
+    }
+
+    public function run() {
+        // todo: json_decode
+        $a = (int) $_REQUEST['a'];
+        $b = (int) $_REQUEST['b'];
+        $c = (int) $_REQUEST['c'];
+
+        $isTriangle = self::isTriangle($a, $b, $c);
+        echo "Треугольник? - " . ($isTriangle ? "Да" : "Нет");
+        if ($isTriangle)
+            echo "<br/>Радиус вписанной окружности = " . self::getRadius($a, $b, $c);
+    }
+}
