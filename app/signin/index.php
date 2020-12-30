eeb96844d1e944ed4792c8428680326c5b7d8ed6
Alexander Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-12-30 14:24:13 +0300

    Вход и регистрация

---
+++ app/signin/index.php
@@ -0,0 +1,45 @@
+<?php
+require_once "../connection.php";
+session_start();
+include "../html.php";
+include "../utils.php";
+
+if (!isset($_SESSION['login'])) { ?>
+<h1 style="text-align: center;">Регистрация</h1>
+<style type="text/css">
+        input {
+            width: 400px;
+        }
+        form {
+            position: relative;
+            left: 40%;
+        }
+    </style>
+<form method="get">
+    <input name="login" type="text" placeholder="Логин" required="true" />
+    <br /><input name="password" type="password" placeholder="Пароль" required="true" />
+    <input name="send" type="hidden" value="true"/>
+    <br /><input type="submit" />
+</form>
+<?php
+    if (isset($_REQUEST['send'])) {
+        $login = $_REQUEST['login'];
+        $password = $_REQUEST['password'];
+        $sql = "select password from users where login = '$login'";
+        $query = Connection::connect()->query($sql);
+        $dbPassword = $query ? $query[0]['password'] : null;
+        p($dbPassword);
+        p($password);
+        p(password_verify($password, $dbPassword) ? "t" : "f");
+        if (!$query) echo p("Неверный логин");
+        else if (!password_verify($password, $dbPassword)) echo p("Неверный пароль");
+        else {
+            $_SESSION['login'] = $login;
+            p("Вы вошли");
+            go(".");
+        }
+    }
+} else {
+    p("Вы уже вошли");
+    // go("/");
+}
---
+++ app/signup/index.php
@@ -0,0 +1,44 @@
+<?php
+require_once "../connection.php";
+require "../utils.php";
+session_start();
+include "../html.php";
+
+if (isset($_SESSION['login'])) {
+    echo "Разлогиньтесь";
+    // go("/");
+} else {
+?>
+<h1 style="text-align: center;">Регистрация</h1>
+<style type="text/css">
+        input {
+            width: 400px;
+        }
+        form {
+            position: relative;
+            left: 40%;
+        }
+    </style>
+<form method="get">
+    <input name="login" type="text" placeholder="Логин" required="true" />
+    <br /><input name="password" type="password" placeholder="Пароль" required="true" />
+    <br /><input name="email" type="email" placeholder="e-mail" required="true" />
+    <br /><input name="name" type="text" placeholder="ФИО" required="true" />
+    <input name="send" type="hidden" value="true"/>
+    <br /><input type="submit" />
+</form>
+<?php
+    if (isset($_REQUEST['send'])) {
+        $login = $_REQUEST['login'];
+        $password = password_hash($_REQUEST['password'], PASSWORD_BCRYPT);
+        $email = $_REQUEST['email'];
+        $name = $_REQUEST['name'];
+
+        $db = Connection::connect();
+        $sql = "insert into users (login, password, pw_hash, name, email, priveleges) values
+        ('$login', '$password', 'bcrypt', '$name', '$email', 'user')";
+        p($sql);
+        $isAdded = $db->query($sql);
+        p("<p>".($isAdded ? "Успешно" : "Ошибка: логин уже занят")."</p>");
+    }
+}
