ca7e2b9e31390cec8009aca0fe05a1494aca78a6
Alexander Kolbasov <kolbasov.a.a@edu.mirea.ru>
2021-01-10 19:45:36 +0300

    Инструменты для парсинга дат

--- app/utils.php
+++ app/utils.php
@@ -5,5 +5,16 @@ function p($string) {
 function go($link) {
     header("Location: $link", true);
 }
-// $timeFormat = "d.m.Y H:i";
-$timeFormat = "d.m.Y";
+
+class Time {
+    public const timeFormat = "d.m.Y";
+    public const fullTimeFormat = "d.m.Y H:i";
+
+    public static function getTimestamp(string $timestampFromDB) : int {
+        return date_timestamp_get(new DateTime($timestampFromDB));
+    }
+
+    public static function format(string $timestampFromDB, string $timeFormat) : string {
+        return date($timeFormat, self::getTimestamp($timestampFromDB));
+    }
+}
